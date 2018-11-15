c570bb1dcc14dd4de4cb6b99310c5e5e13952244
Jared Reando and Katrina Hockman <katrina@>
2018-11-15 11:37:46 -0800

    Finish the thing.

--- lib/allergy_check.rb
+++ lib/allergy_check.rb
@@ -1,31 +1,41 @@
 require('pry')
 
 def allergy_check(score)
-  quarters = 0
-  dimes = 0
-  nickels = 0
-  pennies = 0
+
   answer_array = []
   scratch_paper = score
-  if(scratch_paper / 25 != 0)
-      quarters = scratch_paper /25
-      scratch_paper = scratch_paper % 25
-      answer_array.push("Quarters: " + quarters.to_s)
+
+  if(scratch_paper / 128 != 0)
+      scratch_paper = scratch_paper % 128
+      answer_array.push("cats")
   end
-  if(scratch_paper / 10 != 0)
-      dimes = scratch_paper /10
-      scratch_paper = scratch_paper % 10
-      answer_array.push("Dimes: " + dimes.to_s)
+  if(scratch_paper / 64 != 0)
+      scratch_paper = scratch_paper % 64
+      answer_array.push("pollen")
   end
-  if(scratch_paper / 5 != 0)
-      nickels = scratch_paper / 5
-      scratch_paper = scratch_paper % 5
-      answer_array.push("Nickels: " + nickels.to_s)
+  if(scratch_paper / 32 != 0)
+      scratch_paper = scratch_paper % 32
+      answer_array.push("chocolate")
+  end
+  if(scratch_paper / 16 != 0)
+      scratch_paper = scratch_paper % 16
+      answer_array.push("tomatoes")
+  end
+  if(scratch_paper / 8 != 0)
+      scratch_paper = scratch_paper % 8
+      answer_array.push("strawberries")
+  end
+  if(scratch_paper / 4 != 0)
+      scratch_paper = scratch_paper % 4
+      answer_array.push("shellfish")
+  end
+  if(scratch_paper / 2 != 0)
+      scratch_paper = scratch_paper % 2
+      answer_array.push("peanuts")
   end
   if(scratch_paper / 1 != 0)
-      pennies = scratch_paper / 1
       scratch_paper = scratch_paper % 1
-      answer_array.push("Pennies: " + pennies.to_s)
+      answer_array.push("eggs")
   end
   answer_array.join(", ")
 end
--- spec/allergy_check_spec.rb
+++ spec/allergy_check_spec.rb
@@ -2,13 +2,22 @@ require('rspec')
 require('allergy_check')
 
 
-describe('coin_combo') do
+describe('allergy_check') do
 
   it('returns eggs given a value of 1.') do
-  expect(coin_combo(1)).to(eq('eggs'))
+  expect(allergy_check(1)).to(eq('eggs'))
   end
 
+  it('returns tomaotes given a value of 16.') do
+  expect(allergy_check(16)).to(eq('tomatoes'))
+  end
 
+  it('returns tomaotes AND eggs AAAAND cats given a value of 145.') do
+  expect(allergy_check(145)).to(eq('cats, tomatoes, eggs'))
+  end
 
+  it('returns SO. MANY. ALLERGIES.') do
+  expect(allergy_check(255)).to(eq('cats, pollen, chocolate, tomatoes, strawberries, shellfish, peanuts, eggs'))
+  end
 
 end
